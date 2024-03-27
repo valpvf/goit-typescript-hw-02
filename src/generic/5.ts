04f14517100e531d16ab5872eda12c26efa4a25f
Valerii Pozhydaiev <Valerii.Pozhydaiev@gmail.com>
2024-03-28 00:23:41 +0200

    add part 1 of exercise Generic

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,8 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<T extends object> {
+  [key: string | number | symbol]: T;
 }
 
 export {};
